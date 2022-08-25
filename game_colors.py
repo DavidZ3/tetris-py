09496d709cd54211fad9b70e6cc081e4207e75f2
David Zhou <z5157517@student.unsw.edu.au>
2022-08-26 09:00:51 +1000

    defined some basic game colors

---
+++ game_colors.py
@@ -0,0 +1,13 @@
+'''
+Defines the color code of the colors used in this tetris project to avoid 
+cluttering the main project
+'''
+
+# Theme 1 - May add more themes later and make switching themes a feature
+WHITE  = "#FFFFFF"
+CYAN   = "#44FFFF"
+PINK   = "#FF44FF"
+YELLOW = "#FFFF44"
+GREEN  = "#44FF44"
+ORANGE = "#FF8800"
+RED    = "#F10000"
